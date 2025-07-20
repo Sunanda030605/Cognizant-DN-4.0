60f577fc34b3626df5a88ac71c4f704ffedae96c
Sunanda N <164130059+Sunanda030605@users.noreply.github.com>
2025-07-20 18:20:49 +0530

    Adding Week 5 Hands-on

---
+++ Week 5 - MicroServices/Microservices_Exercise - 1/SecureController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("protected")]
+        public IActionResult GetSecret()
+        {
+            return Ok("This is a protected endpoint. You are authenticated.");
+        }
+    }
+}
